bf7ecf3e46195dc3b79136c7377021bd3bdb4c49
Lmssn04 <82818828+LouisMasson@users.noreply.github.com>
2024-12-06 22:30:22 +0000

    Change : new plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <head>
         <PlausibleProvider 
           domain="louismasson.patronusguardian.org"
-          customDomain="http://plausible-pcocwko0csw0gkgc0gs0ggc8.100.42.177.245.sslip.io"
+          customDomain="https://analytics.patronusguardian.org"
           trackOutboundLinks={true}
           enabled={process.env.NODE_ENV === 'production'}
         />
